8b3a995816026d60a387c19469c5e5060533d5a3
TheTrueLiberty <podval.saino2@gmail.com>
2026-02-10 07:34:01 -0800

    Add files via upload

---
+++ webmanifest.php
@@ -0,0 +1,35 @@
+{
+    "name": "OPUS - \u041c\u044b \u0442\u0443\u0442!",
+    "short_name": "\u0440\u0443\u0441\u0441\u043a\u043e\u044f\u0437\u044b\u0447\u043d\u044b\u0439 \u0444\u043e\u0440\u0443\u043c, \u0432\u0441\u0435 \u0441\u0438\u0434\u044f\u0442 \u0442\u0443\u0442",
+    "description": "<meta name=\"description\" content=\"opusgu.ru - \u0440\u0443\u0441\u0441\u043a\u043e\u044f\u0437\u044b\u0447\u043d\u044b\u0439 \u0444\u043e\u0440\u0443\u043c, \u0432\u0441\u0435 \u0441\u0438\u0434\u044f\u0442 \u0442\u0443\u0442.",
+    "icons": [
+        {
+            "src": "\/data\/assets\/logo\/1000006443.png",
+            "sizes": "192x192",
+            "purpose": "any"
+        },
+        {
+            "src": "\/data\/assets\/logo\/1000006443.png",
+            "sizes": "192x192",
+            "purpose": "maskable"
+        },
+        {
+            "src": "\/data\/assets\/logo\/1000006443.png",
+            "sizes": "512x512",
+            "purpose": "any"
+        },
+        {
+            "src": "\/data\/assets\/logo\/1000006443.png",
+            "sizes": "512x512",
+            "purpose": "maskable"
+        }
+    ],
+    "lang": "ru-RU",
+    "dir": "ltr",
+    "display": "standalone",
+    "scope": "\/",
+    "id": "\/index.php?_pwa=1",
+    "start_url": "\/index.php?_pwa=1",
+    "background_color": "#000000",
+    "theme_color": "#ffffff"
+}
